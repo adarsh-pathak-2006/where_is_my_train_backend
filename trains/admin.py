f7c08dd2c7cc02f3a2babbf458a78ae089a7816f
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-13 01:09:09 +0530

    added trains models

--- trains/admin.py
+++ trains/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Train, TrainStation
 
-# Register your models here.
+admin.site.register(TrainStation)
+admin.site.register(Train)
--- trains/models.py
+++ trains/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from station.models import Station
 
-# Create your models here.
+class Train(models.Model):
+    name=models.CharField(max_length=300)
+    starting_station=models.ForeignKey(Station, on_delete=models.CASCADE, related_name='trains_starting')
+    ending_station=models.ForeignKey(Station, on_delete=models.CASCADE, related_name='trains_ending')
+    train_type=models.CharField(max_length=15, choices=[('SUPERFAST', 'SUPERFAST'), ('PASSENGER', 'PASSENGER'), ('MEMU', 'MEMU'), ('EXPRESS', 'EXPRESS')])
+    added_on=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class TrainStation(models.Model):
+    train=models.ForeignKey(Train, on_delete=models.CASCADE, related_name='stations')
+    station=models.ForeignKey(Station, on_delete=models.CASCADE, related_name='train_stations')
+    stopping_time=models.DurationField()
+    distance_from_origin=models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"train {self.train.name} at {self.station.name}"
+
