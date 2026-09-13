936ba2f30279cd19222efb73a1ca4256838d8c12
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-13 15:56:39 +0530

    removed extra app for now

--- wimt/settings.py
+++ wimt/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'core', 'trains', 'station'
+    'trains', 'station'
 ]
 
 MIDDLEWARE = [
