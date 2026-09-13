7d44809f70d815f70cf5c52f357b42363409fd48
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-13 16:31:57 +0530

    all endpoints working nice

--- trains/views.py
+++ trains/views.py
@@ -13,12 +13,12 @@ from rest_framework.permissions import AllowAny
 class AllTrainAPI(APIView):
     permission_classes=[AllowAny]
     def get(self, request):
-        page_no=request.query_param.get("page", "1")
+        page_no=request.query_params.get("page", "1")
         cached_data=cache.get(train_list_cache_key(pageno=page_no))
         if cached_data:
             return Response(cached_data, status=200)
         paginator=GeneralPagination()
-        queryset=paginator.paginate_queryset(Train.objects.select_related('trains_starting', 'trains_ending').all(), request, view=self)
+        queryset=paginator.paginate_queryset(Train.objects.select_related('starting_station', 'ending_station').all(), request, view=self)
         serial=TrainSerializer(queryset, many=True)
         response=paginator.get_paginated_response(serial.data)
         cache.set(train_list_cache_key(pageno=page_no), response.data, timeout=6000)
@@ -30,7 +30,7 @@ class TrainStationAPI(APIView):
         cached_data=cache.get(train_all_station_cache_key(train_id=pk))
         if cached_data:
             return Response(cached_data, status=200)
-        data=TrainStation.objects.select_related('train__id').filter(train__id=pk)
+        data=TrainStation.objects.select_related('train').filter(train__id=pk)
         serial=TrainStationSerializer(data, many=True)
         cache.set(train_all_station_cache_key(train_id=pk), serial.data, timeout=6000)
         return Response(serial.data, status=200)
