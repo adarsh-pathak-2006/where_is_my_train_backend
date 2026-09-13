080955e3cc320b0c135fa664cc42e36533c23799
duckyduck01 <madhvetipathak2006@gmail.com>
2026-09-13 23:13:27 +0530

    added views in the core app

--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import CurrentStatus
+from .serializers import CurrentStationGetSerializer, CurrentStationSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAdminUser
 
-# Create your views here.
+class CurrentStatusAPI(APIView):
+    def get_permissions(self):
+        if self.request.method=='GET':
+            return [AllowAny()]
+        return [IsAdminUser()]
+    def get(self, request, name):
+        data=get_object_or_404(CurrentStatus.objects.select_related('train', 'previous_station', 'upcoming_station', 'nearest_station'), train__name=name)
+        serial=CurrentStationGetSerializer(data)
+        return Response(serial.data, status=200)
+
+    def post(self, request, name):
+        serial=CurrentStationSerializer(data=request.data)
+        if serial.is_valid():
+            serial.save()
+            return Response(serial.data, status=201)
+        return Response(serial.errors, status=400)
